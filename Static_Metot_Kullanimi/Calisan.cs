9e46fb1cdfdc55b71f88d071ae26ddfbe7d843ac
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-04 15:31:25 +0300

    Add project files.

---
+++ Static_Metot_Kullanimi/Calisan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Metot_Kullanimi
+{
+    public class Calisan
+    {
+        public int calisanID { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+
+        public static int maas { get; set; }
+
+    }
+}
---
+++ Static_Metot_Kullanimi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Metot_Kullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan C1 = new Calisan();
+
+            C1.calisanID = 1;
+            C1.isim = "Pınar";
+            C1.soyisim = "ALTINKAYA";
+            Calisan.maas = 200000; // static olduğu zaman class ismi üzerinden erişim sağladım
+
+
+            Console.WriteLine("Çalisan id : "+ C1.calisanID);
+            Console.WriteLine("Calisan isim : "+ C1.isim);
+            Console.WriteLine("Calisan soyisim "+C1.soyisim);
+            Console.WriteLine("Calisan maas : "+ Calisan.maas);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
